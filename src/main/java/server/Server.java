c00b56a36a8ffdbbceff71b9bfb84270803fbcc9
DmitriyChistyakov <dinacher@mail.ru>
2021-07-09 16:20:00 +0300

    homework1 Передача файлов на сервер.

---
+++ src/main/java/server/Server.java
@@ -0,0 +1,25 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(8189);
+        System.out.println("Server started....");
+        while (true) {
+            Socket socket = server.accept(); //ожидание клиента
+            System.out.println("Client accepted.....");
+            try {
+                new Thread(new Handler(socket)).start();
+
+            } catch (Exception e) {
+                System.err.println("Connection error.");
+
+            }
+
+        }
+    }
+}
